175dd337a87e5ad3a146e2968391aaa0f06fd4aa
Weslley <weslleyzh@hotmail.com>
2022-02-07 09:52:29 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.bars')
+
+hamburger.addEventListener('click', evento => {
+  evento.target.classList.toggle('show')
+  document.querySelector('.nav').classList.toggle('show')
+})
+
+let nextBtn = document.querySelectorAll('#next')
+let backBtn = document.querySelectorAll('#back')
+
+for (let i = 0; i < nextBtn.length; i++) {
+  nextBtn[i].addEventListener('click', e => {
+    e.target.parentElement.parentElement.children[2].scrollBy(100, 0)
+  })
+  backBtn[i].addEventListener('click', e => {
+    e.target.parentElement.parentElement.children[2].scrollBy(-100, 0)
+  })
+}
